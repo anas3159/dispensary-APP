d3c9ee16ed5daf6cf73c038c76a30cc64a8bfb05
anas3159 <aab28622@gmail.com>
2026-05-06 19:59:31 +0300

    update: fix mongo connection and other changes

--- Controller/usersController.js
+++ Controller/usersController.js
@@ -1,4 +1,19 @@
-const user = "../models/userModel.js";
-const getAlluser = (req, res) => {
-  res.json({ status: "success", data: { user } });
+const userModel = require("../models/userModel");
+
+const getAlluser = async (req, res) => {
+  try {
+    const users = await userModel.find(); // جلب كل المستخدمين
+
+    res.status(200).json({
+      status: "success",
+      data: users,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: error.message,
+    });
+  }
 };
+
+module.exports = { getAlluser };
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const url = process.env.MONGO_URL;
 mongoose.connect(url).then(() => {
   console.log("server started");
 });
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 const usersRouter = require("./routes/usersRoutes");
 app.use("/api/users", usersRouter);
