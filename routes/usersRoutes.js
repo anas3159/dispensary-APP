c48d35dc09b4c68ba569b89da087143390b92330
anas3159 <aab28622@gmail.com>
2026-05-06 20:59:14 +0300

    reslove

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const usersController = require("../controller/usersController");
+const usersController = require("../Controller/usersController");
 router.route("/").get(usersController.getAlluser);
 module.exports = router;
